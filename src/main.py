1f3a1636e12ceeee70de72651d615bf8b2903f0d
spez1998 <samman30@live.com>
2020-09-04 13:12:18 +0100

    Added comments, cleaned up output

--- src/main.py
+++ src/main.py
@@ -3,10 +3,11 @@
 import sys
 import toml
 
-usr_config = toml.load("../test_config.toml")
+usr_config = toml.load("../test_config.toml") # Load user config into dict
 
 count = 1
 
+# Check if requested section exists in config
 if sys.argv[1] in usr_config.keys():
     module = sys.argv[1]
 else:
@@ -14,9 +15,14 @@ else:
     exit()
 
 count = 1
+print("Press Enter to toggle module behaviour\n")
+
+#Toggling between different subsections
 while True:
-    input("Press Enter to toggle module behaviour\n")
-    print(usr_config.get(module).get(str(count)))
+    input()
+    submodule = usr_config.get(module).get(str(count)) # Get next submodule down from config list
+    print(toml.dumps(submodule)) # Print next submodule down in TOML format
+    # Keep iterating through submodules, loop back to top of list if bottom is reached
     if count == 1:
         count += 1
     else:
