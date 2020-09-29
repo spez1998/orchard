ecf75ea6382363e8e9ba89514fff1a8d8664284e
spez1998 <samman30@live.com>
2020-09-29 21:25:13 +0100

    No changes - some lines commented.

--- src/compare_toggle.py
+++ src/compare_toggle.py
@@ -25,17 +25,17 @@ while True:
     # Check if currently displayed module is the same as first in user config
     #if current_submodule == req_submodule:
     #    count += 1
-    print(usr_config)
+    #print(usr_config)
     count += 1
     if current_submodule == req_submodule:
         count += 1
     req_submodule = usr_config.get(module).get(str(count))
-    print(req_submodule)
+    #print(req_submodule)
     current_config[module] = req_submodule
     #print(current_config)
     with open('output.toml', "w") as toml_file:
         toml.dump(current_config,toml_file)
-    # Print next submodule down in TOML format
+        # Print next submodule down in TOML format
     ##########print(to_dump := "[{}]\n{}".format(module,toml.dumps(req_submodule)))
     # print(to_dump_dict := {'['module']': {'path': toml.dumps(req_submodule), 'reload': '1'}}
     #toml.dump(to_dump,"./output.toml")
