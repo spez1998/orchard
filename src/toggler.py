8cdef7b57551b1c69a00da0879529a61e7072bd3
Sujit Malde <samman30@live.com>
2021-03-07 10:55:53 -0800

    Always loops through available list of submodules infinitely

--- src/toggler.py
+++ src/toggler.py
@@ -3,21 +3,25 @@
 import sys
 import toml
 
-usr_config = toml.load("../config/test_config.toml")            # Load user config into dict
+usr_config = toml.load("../config/test_config.toml")
 count = 1
-if sys.argv[1] in usr_config.keys():                            # Check if requested module exists in config
-    module = sys.argv[1]
+arg = sys.argv[1].split(".")
+
+if arg[0] in usr_config.keys():
+    module = arg[0]
+    submodule_num = arg[1]
 else:
     print("Module not specified in config.toml")
     exit()
 
-count = 1
-while True:                                                     # Toggling between different subsections
-    input("Press Enter to toggle module behaviour\n")
-    # print(usr_config.get())
-    submodule = dict(usr_config.get(module).get(str(count)))    # Get next submodule down from config list
-    print(f"[{module}]\n{toml.dumps(submodule)}")               # Print next submodule down in TOML format
-    if count == 1:                                              # Keep looping through submodules
-        count += 1
-    else:
-        count = 1
+print(f"You are using date.{submodule_num}")
+
+count = int(submodule_num) # Start at current submodule
+while True:
+    input("Press Enter to go to next module\n")
+    num_variants = len(usr_config.get(module).keys())
+    next_submodule_num = (count%num_variants)+1 # Get next number down, loop to top of list if at bottom
+    submodule = dict(usr_config.get(module).get(str(next_submodule_num))) # Get next submodule down from config list
+    print(f"[{module}]\n{toml.dumps(submodule)}")
+    print(f"You are now using date.{next_submodule_num}")
+    count += 1
